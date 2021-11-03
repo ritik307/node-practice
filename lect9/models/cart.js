8eaa9fd5929aef1c6c7e7939335f566db3095c99
Ritik <ritikpr307@gmail.com>
2021-11-03 15:39:08 +0530

    lect9-complete

--- lect9/models/cart.js
+++ lect9/models/cart.js
@@ -1,3 +1,4 @@
+const { deepStrictEqual } = require("assert");
 const fs=require("fs");
 const path=require("path");
 
@@ -38,6 +39,40 @@ module.exports = class Cart{
                 console.log(err);
             })
         })
-        
+
+    }
+    static deleteProduct(id,productPrice){
+        fs.readFile(p,(err,fileContent)=>{
+            if(err){
+                return;
+            }
+            const updatedCart = {...JSON.parse(fileContent)};
+            const product = updatedCart.products.find(prod=>prod.id===id);
+            if(!product){
+                return;
+            }
+            const productQty=product.qty;
+            updatedCart.products=updatedCart.products.filter(
+                prod=>prod.id!==id
+            );
+            updatedCart.totalPrice=updatedCart.totalPrice-(productPrice*productQty);
+
+            fs.writeFile(p,JSON.stringify(updatedCart),err=>{
+                console.log(err);
+            });
+
+            
+        });  
+    }
+    static getCart(cb){
+        fs.readFile(p,(err,fileContent)=>{
+            const cart = JSON.parse(fileContent);
+            if(err){
+                cb(null);
+            }
+            else{
+                cb(cart);
+            }
+        });           
     }
 };
