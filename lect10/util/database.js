7f74dce1577eb800105cc16b12d19730ab00eea9
Ritik <ritikpr307@gmail.com>
2021-11-06 21:42:35 +0530

    lect10-error

---
+++ lect10/util/database.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+
+//? conenction pool allow us to use multiple connections
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    database: "node-complete",
+    password: "root"
+});
+
+//? allows us to use promises when working with these conenctions which handles async tasks instead of callbacks
+//? bcz promises allows us to write code in a bit more structured way. NO NESTED CALLBACKS(callback hell)
+module.exports=pool.promise();
