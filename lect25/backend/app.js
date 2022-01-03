fff358f95ed2ec24a4931de679a4925123421075
ritik307 <ritikpr307@gmail.com>
2022-01-03 20:02:43 +0530

    lect25-385

--- lect25/backend/app.js
+++ lect25/backend/app.js
@@ -6,6 +6,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
+
 const mongoose = require("mongoose");
 
 const multer = require("multer");
@@ -54,13 +56,15 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth",authRoutes);
 
 //? General error handler
 app.use((error, req, res, next) => {
   console.log("ERROR: ", error);
   const status = error.statusCode || 500;
   const message = error.message;
-  res.status(status).json({ message: message });
+  const data = error.data;
+  res.status(status).json({ message: message, data:data });
 });
 
 mongoose
